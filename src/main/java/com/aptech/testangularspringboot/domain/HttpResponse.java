a8b037958746427a450a5f6e37be631cc27dff57
tin <tinsatthu1988@gmail.com>
2021-12-05 22:38:16 +0700

    036 Test endpoint for security

--- src/main/java/com/aptech/testangularspringboot/domain/HttpResponse.java
+++ src/main/java/com/aptech/testangularspringboot/domain/HttpResponse.java
@@ -1,5 +1,6 @@
 package com.aptech.testangularspringboot.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
@@ -9,6 +10,7 @@ import java.util.Date;
 @Getter
 @Setter
 public class HttpResponse {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM-dd-yyyy hh:mm:ss", timezone = "America/New_York")
     private Date timeStamp;
     private int httpStatusCode; // 200, 201, 400, 500
     private HttpStatus httpStatus;
