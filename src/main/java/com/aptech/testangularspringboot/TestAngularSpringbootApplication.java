6266af935b79614f677b22f5fcefd82703c3ce00
tin <tinsatthu1988@gmail.com>
2021-12-05 22:23:09 +0700

    035 Security configuration

--- src/main/java/com/aptech/testangularspringboot/TestAngularSpringbootApplication.java
+++ src/main/java/com/aptech/testangularspringboot/TestAngularSpringbootApplication.java
@@ -2,6 +2,8 @@ package com.aptech.testangularspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class TestAngularSpringbootApplication {
@@ -10,4 +12,8 @@ public class TestAngularSpringbootApplication {
 		SpringApplication.run(TestAngularSpringbootApplication.class, args);
 	}
 
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
--- src/main/java/com/aptech/testangularspringboot/configuration/MyDataRestConfig.java
+++ src/main/java/com/aptech/testangularspringboot/configuration/MyDataRestConfig.java
@@ -1,11 +1,9 @@
-package com.aptech.testangularspringboot.config;
+package com.aptech.testangularspringboot.configuration;
 
-import com.aptech.testangularspringboot.entity.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import javax.persistence.EntityManager;
