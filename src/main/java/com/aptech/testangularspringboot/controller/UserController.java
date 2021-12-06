ac9a59275f6428e1fa78d9209f5ba09032a05c05
tin <tinsatthu1988@gmail.com>
2021-12-06 22:21:00 +0700

    041 Custom exception handling

--- src/main/java/com/aptech/testangularspringboot/controller/UserController.java
+++ src/main/java/com/aptech/testangularspringboot/controller/UserController.java
@@ -1,12 +1,13 @@
 package com.aptech.testangularspringboot.controller;
 
+import com.aptech.testangularspringboot.exception.ExceptionHandling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
-public class UserController {
+public class UserController extends ExceptionHandling {
 
     @GetMapping("/home")
     public String showUser(){
