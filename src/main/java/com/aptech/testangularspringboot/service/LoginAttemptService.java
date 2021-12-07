9bcfe83588715ce237c4cca0019fc3828dc85817
tin <tinsatthu1988@gmail.com>
2021-12-07 22:50:24 +0700

    059 Brute force attack cache

---
+++ src/main/java/com/aptech/testangularspringboot/service/LoginAttemptService.java
@@ -0,0 +1,44 @@
+package com.aptech.testangularspringboot.service;
+
+
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+import static java.util.concurrent.TimeUnit.MINUTES;
+
+@Service
+public class LoginAttemptService {
+    private static final int MAXiMUM_NUMBER_OF_ATTEMPTS = 5;
+    private static final int ATTEMPT_INCREMENT= 1;
+    private LoadingCache<String, Integer> loginAttemptCache;
+
+    public LoginAttemptService(){
+        super();
+        loginAttemptCache = CacheBuilder.newBuilder().expireAfterWrite(15, MINUTES)
+                .maximumSize(100).build(new CacheLoader<String, Integer>(){
+                    public Integer load(String key){
+                        return 0;
+                    }
+                });
+    }
+
+    public void evictUserFromLoginAttemptCache(String username){
+        loginAttemptCache.invalidate(username);
+    }
+
+    public void addUserToLoginAttemptCache(String username) throws ExecutionException {
+        int attempts = 0;
+        attempts = ATTEMPT_INCREMENT + loginAttemptCache.get(username);
+        loginAttemptCache.put(username, attempts);
+    }
+
+    public boolean hasExceededMaxAttempts(String username) throws ExecutionException {
+        return loginAttemptCache.get(username) >= MAXiMUM_NUMBER_OF_ATTEMPTS;
+    }
+}
