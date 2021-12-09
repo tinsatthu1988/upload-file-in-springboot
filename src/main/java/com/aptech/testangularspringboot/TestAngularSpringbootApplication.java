fa5fc6fa796400df82f60b43b5749055b940f4c1
TIN-THIET-KE <TIN-THIET-KE@TIN-THIET-KE>
2021-12-09 11:31:12 +0700

    071 File Constant

--- src/main/java/com/aptech/testangularspringboot/TestAngularSpringbootApplication.java
+++ src/main/java/com/aptech/testangularspringboot/TestAngularSpringbootApplication.java
@@ -5,11 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.io.File;
+
+import static com.aptech.testangularspringboot.constant.FileConstant.USER_FOLDER;
+
 @SpringBootApplication
 public class TestAngularSpringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestAngularSpringbootApplication.class, args);
+		new File(USER_FOLDER).mkdirs();
 	}
 
 	@Bean
