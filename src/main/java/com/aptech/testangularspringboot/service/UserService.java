b214c469915c5e19821e10f46047a0e50d6a21fd
TIN-THIET-KE <TIN-THIET-KE@TIN-THIET-KE>
2021-12-09 10:26:06 +0700

    070 User service

--- src/main/java/com/aptech/testangularspringboot/service/UserService.java
+++ src/main/java/com/aptech/testangularspringboot/service/UserService.java
@@ -2,6 +2,7 @@ package com.aptech.testangularspringboot.service;
 
 import com.aptech.testangularspringboot.domain.User;
 import com.aptech.testangularspringboot.exception.domain.EmailExistException;
+import com.aptech.testangularspringboot.exception.domain.EmailNotFoundException;
 import com.aptech.testangularspringboot.exception.domain.UserNotFoundException;
 import com.aptech.testangularspringboot.exception.domain.UsernameExistException;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,5 +19,15 @@ public interface UserService {
     User findUserByUsername(String username);
 
     User findUserByEmail(String email);
+
+    User addNewUser(String firstName, String lastName, String username, String email, String role, boolean isNonLocked, boolean isActive, MultipartFile profileImage) throws UserNotFoundException, UsernameExistException, EmailExistException, IOException, NotAnImageFileException;
+
+    User updateUser(String currentUsername, String newFirstName, String newLastName, String newUsername, String newEmail, String role, boolean isNonLocked, boolean isActive, MultipartFile profileImage) throws UserNotFoundException, UsernameExistException, EmailExistException, IOException, NotAnImageFileException;
+
+    void deleteUser(String username) throws IOException;
+
+    void resetPassword(String email) throws MessagingException, EmailNotFoundException;
+
+    User updateProfileImage(String username, MultipartFile profileImage) throws UserNotFoundException, UsernameExistException, EmailExistException, IOException;
 
 }
